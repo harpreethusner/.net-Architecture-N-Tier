a22be6fe790d1c90b023e82644783f8d88316f0c
harpreethusner <harpreethusner@gmail.com>
2026-01-26 17:57:06 +0530

    Update comment for HomeController

--- CRUD-N-Tier/Controllers/HomeController.cs
+++ CRUD-N-Tier/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 namespace CRUD_N_Tier.Controllers
 {
 
-    // Home controller 
+    // deploy branch Home controller 
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
